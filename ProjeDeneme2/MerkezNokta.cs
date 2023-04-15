d9da4923087c5d78697c4528827e3994d2f282b6
NulliussInVerba <veysel.aras@ogr.sakarya.edu.tr>
2023-04-15 16:55:05 +0300

    Add project files.

---
+++ ProjeDeneme2/MerkezNokta.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeDeneme2
+{
+    internal class MerkezNokta
+    {
+        public int x
+        {
+            set
+            {
+                x = value;
+            }
+        }
+        public int merkezY
+        {
+            set
+            {
+                merkezY = value;
+            }
+        }
+        //x ve y atandı
+    }
+
+    internal class Cember : MerkezNokta
+    {
+        public int yaricap
+        {
+            set
+            {
+                yaricap = value;
+            }
+        }
+    }
+    //r atandı
+
+    internal class Kure : Cember
+    {
+        public int merkezZ
+        {
+            set
+            {
+                merkezZ = value;
+            }
+        }
+    }
+    //Kure tamam
+
+    internal class Silindir : Cember
+    {
+        public int merkezZ
+        {
+            set
+            {
+                merkezZ = value;
+            }
+        }
+        public int Yukseklik
+        {
+            set
+            {
+                Yukseklik = value;
+            }
+        }
+    }
+    //Silindir tamam
+
+    internal class Dortgen : MerkezNokta
+    {
+        public int boy
+        {
+            set
+            {
+                boy = value;
+            }
+        }
+        public int en
+        {
+            set
+            {
+                en = value;
+            }
+        }
+    }
+    //Dortgen tamam
+
+    internal class DikdortgenPrizma : Dortgen
+    {
+        public int merkezZ
+        {
+            set
+            {
+                merkezZ = value;
+            }
+        }
+        public int derinlik
+        {
+            set
+            {
+                derinlik = value;
+            }
+        }
+    }
+    //Dikdörtgen prizma tamam
+}
---
+++ ProjeDeneme2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjeDeneme2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        void Menu()
+        {
+            //En son oluşturulacak 
+        }
+    }
+}
