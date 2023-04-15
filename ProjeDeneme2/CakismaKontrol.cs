52b3bfe2c3b22df7609915d66e1dc6465768ab3e
NulliussInVerba <veysel.aras@ogr.sakarya.edu.tr>
2023-04-15 22:04:52 +0300

    Eğim bulundu x veya y bulundu şimdi pisagor ile çakışma var mı kontrol edilecek

--- ProjeDeneme2/CakismaKontrol.cs
+++ ProjeDeneme2/CakismaKontrol.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace ProjeDeneme2
 {
-    static class CakismaKontrol
+    public static class CakismaKontrol
     {
-        
+        //Nokta Dörtgen Çakışma Kontrolü
+        public static bool noktaDortgen(int noktaX, int noktaY, int dortgenX, int dortgenY, int dortgenEn, int dortgenBoy)
+        {
+            int dogruY, pisagorBir, pisagorIki,dogruX;
+            int egim = (noktaY - dortgenY) / (noktaX - dortgenX);
+            bool noktaXbuyuk, noktaYBuyuk;
+
+            if (Math.Abs(noktaX - dortgenX) > dortgenEn / 2)
+            {
+                if (noktaX > dortgenX)
+                {
+                    dogruY = ((dortgenX + dortgenEn / 2) - dortgenX) * egim + dortgenY;
+                }
+                else
+                {
+                    dogruY = ((dortgenX - dortgenEn / 2) - dortgenX) * egim + dortgenY;
+                }
+            }
+            else if((noktaY - dortgenY) > dortgenBoy / 2)
+            {//HATALI nokta büyükse öbür yden alacağımız x koordinatını bulmaılıyız y değil
+                if (noktaY > dortgenY)
+                {
+                    dogruX = ((dortgenY + dortgenBoy / 2) - dortgenY) / egim + dortgenX;
+                }
+                else
+                {
+                    dogruX = ((dortgenY - dortgenBoy / 2) - dortgenY) / egim + dortgenX;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Çakışma Vardır");
+            }
+
+
+            return true;
+        }
     }
 }
